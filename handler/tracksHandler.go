77d8a51749dcf65d4ec387b5617ca2cf08ecfdd2
Keita <bonjovi1926@yahoo.co.jp>
2017-12-26 13:33:15 +0900

    add: tracksHandler

---
+++ handler/tracksHandler.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"github.com/labstack/echo"
+	"encoding/json"
+	"net/http"
+)
+
+func GetTracksHandler(c echo.Context) error {
+
+	tracks := []string{"3n3Ppam7vgaVa1iaRUc9Lp"}
+
+	res, err := Token.GetTracks(tracks)
+	if err != nil {
+		return err
+	}
+	out, err := json.Marshal(res)
+	if err != nil {
+		return err
+	}
+
+	return c.String(http.StatusOK, string(out))
+}
